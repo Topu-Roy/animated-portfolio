bc323212f2a79a4341f816c6fa2dc070cb4c6f01
Topu-Roy <topuroy143333@gmail.com>
2024-06-18 00:26:44 +0600

    Installed magic ui, shadcn ui and basic setup for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,20 @@
-import Link from "next/link";
+import { OrbitingIcons } from "@/components/home/orbitDemo";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <main className="mx-auto flex min-h-screen w-full max-w-[90rem] flex-col items-center justify-center">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-8">
+          <h1 className="text-balance text-7xl font-semibold">
+            Award-winning development studio based in Denmark.
+          </h1>
+          <p className="w-[80%] text-xl leading-8 text-black/70">
+            We are a development studio working at the intersection of design
+            and technology. It&apos;s a really busy intersection though — a lot
+            of our staff have been involved in hit and runs.
+          </p>
         </div>
+        <OrbitingIcons />
       </div>
     </main>
   );
